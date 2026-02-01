f697b326e0d89672837273fb990ad2deafddd6bb
natively.dev <api@natively.dev>
2026-02-01 05:39:06 +0000

    fix: Correct Reanimated Babel plugin in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+
 module.exports = function (api) {
   api.cache(true);
 
@@ -41,7 +42,9 @@ module.exports = function (api) {
       ],
       ...EDITABLE_COMPONENTS,
       "@babel/plugin-proposal-export-namespace-from",
-      "react-native-worklets/plugin", // react-native-worklets/plugin must be listed last!
+
+      // 🚨 MUST be last
+      "react-native-reanimated/plugin",
     ],
   };
 };
